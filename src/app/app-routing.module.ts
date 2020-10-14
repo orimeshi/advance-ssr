a04883a7d26418858e13a86f96d441429b508cfe
Ori Meshi <silkroadori@gmail.comgit config --global user.email silkroadori@gmail.com>
2020-10-14 12:24:16 +0300

    da

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -22,7 +22,7 @@ const routes: Routes = [
   {path: 'sea', component: SeaComponent},
   {path: 'travel', component: TravelComponent},
   {path: 'property', component: PropertyComponent},
-  {path: 'thanks', component: ThanksComponent}
+  {path: 'thankyoupage', component: ThanksComponent}
 ];
 
 @NgModule({
