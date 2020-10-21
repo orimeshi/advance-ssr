b7b6cbd1c5739d31aad11c37cffd9d5d2ef02f7b
Ori Meshi <silkroadori@gmail.comgit config --global user.email silkroadori@gmail.com>
2020-10-21 11:35:33 +0300

    sitemap

--- src/app/online/online.component.ts
+++ src/app/online/online.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import * as $ from 'jquery/dist/jquery.min.js';
+
 @Component({
   selector: 'app-online',
   templateUrl: './online.component.html',
@@ -14,10 +16,16 @@ title = 'ביטוח אונליין - איילת דן אדוונס ביטוחים
     alert('פנייתך התקבלה בהצלחה!');
   
   }
+  toggleModal(){
+    var modal = $('.modal');
+   
+    modal.toggleClass('is-active');
+  }
 
 
   ngOnInit(){
     this.titleService.setTitle(this.title);
+  
   }
 
 }
